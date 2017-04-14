8b20eceb072b65c4388f76f953833173537be987
GauravK <gaurav.keshre@gmail.com>
2017-04-15 02:15:38 +0530

    fixed a scenario when next pushed vc does not work properly due to gesture delegate in main class

--- InteractivePopDemo/ViewController.swift
+++ InteractivePopDemo/ViewController.swift
@@ -38,13 +38,21 @@ class DetailVC: UIViewController, UIGestureRecognizerDelegate {
     //--------------------------
     func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
 
+        /** We want to intercept the gesture only in case it is `interactivePopGestureRecognizer` */
+
         guard gestureRecognizer.isEqual(self.navigationController?.interactivePopGestureRecognizer) else{ return true }
         
-        /** This is our interactive pop gesture */
+        /** We don't want all the subsequently pushed controllers to behave. We want to intercept this only for this `view` */
+
+        guard gestureRecognizer.view == self.view else {return true}
+    
+        /** This logic may change a bit for accomodating your use case. But I just want to disable interactive pop for bottom half of the screen*/
+        
         let pointofTouch = gestureRecognizer.location(in: self.view)
         
-        /** This logic may change a bit for accomodating your use case. But I just want to disable interactive pop for bottom half of the screen*/
         let isTouchInBottomHalf = (pointofTouch.y >= self.view.bounds.height / 2)
+        
         return !isTouchInBottomHalf
+        
     }
 }
