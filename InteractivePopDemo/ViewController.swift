95b9f2c5aea6c658dc5381fdb90c29743bda4eb4
GauravK <gaurav.keshre@gmail.com>
2017-04-17 15:06:26 +0530

    added a case for scrollable content

--- InteractivePopDemo/ViewController.swift
+++ InteractivePopDemo/ViewController.swift
@@ -24,11 +24,6 @@ class MasterVC: UIViewController {
 
 class DetailVC: UIViewController, UIGestureRecognizerDelegate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.interactivePopGestureRecognizer?.delegate = self
@@ -53,6 +48,50 @@ class DetailVC: UIViewController, UIGestureRecognizerDelegate {
         /** This logic may change a bit for accomodating your use case. But I just want to disable interactive pop for bottom half of the screen*/
         
         let pointofTouch = gestureRecognizer.location(in: self.view)
+        
+        let isTouchInBottomHalf = (pointofTouch.y >= self.view.bounds.height / 2)
+        
+        return !isTouchInBottomHalf
+        
+    }
+}
+
+
+
+class DetailTVC: UITableViewController, UIGestureRecognizerDelegate{
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self
+    }
+   
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
+    }
+
+    
+    //MARK:- UIGestureRecognizerDelegate Methods
+    //--------------------------
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        /** We want to intercept the gesture only in case it is `interactivePopGestureRecognizer` */
+        
+        guard gestureRecognizer.isEqual(self.navigationController?.interactivePopGestureRecognizer) else{ return true }
+        
+        /** This logic may change a bit for accomodating your use case. But I just want to disable interactive pop for bottom half of the screen*/
+        
+        let pointofTouch = gestureRecognizer.location(in: self.view)
+        
+        let indexPath = IndexPath(row: 3, section: 0)
+        let rect = tableView.rectForRow(at: indexPath)
+        
+        if rect.contains(pointofTouch){
+            print("trying to swipe on scrollable content")
+            return false
+        }else{
+            print("it's okay")
+            return true
+        }
         
         let isTouchInBottomHalf = (pointofTouch.y >= self.view.bounds.height / 2)
         
